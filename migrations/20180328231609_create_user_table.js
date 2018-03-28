e6a1932fc014032fd8fc4c49c7be2b9ab6b5190a
Vahe001 <Vahemelqon96@gmail.com>
2018-03-29 00:32:36 +0400

    Some changes

--- migrations/20180328231609_create_user_table.js
+++ migrations/20180328231609_create_user_table.js
@@ -1,11 +1,12 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTableIfNotExists('user', function (t) {
+    return knex.schema.createTableIfNotExists('users', function (t) {
         t.increments('id').primary()
         t.string('username').notNullable().unique().collate('utf8_unicode_ci');
         t.string('password').notNullable()
         t.string('name').notNullable()
         t.string('email').unique().collate('utf8_unicode_ci');
+        t.string('role').default('user')
     })
 };
 
